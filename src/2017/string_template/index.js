9f791c79a91177c45fffcd8ac5959545d7caedb9
leegsen7 <1140313001@qq.com>
2018-03-26 21:21:39 +0800

    update string_template reg

--- src/2017/string_template/index.js
+++ src/2017/string_template/index.js
@@ -7,19 +7,29 @@ let data = {
 }
 
 function TemplateEngine(str,data){
-	let reg = /\${([^\}]+)?\}/g,
-        code = 'var r=[];\n',
-        cursor = 0;
+	let reg = /\${([^\}]+)?\}/g
+    let code = 'var r=[];\n'
+    let cursor = 0
+    let match = null
     var add = function(line,flag){
     	flag ? code += 'r.push(this.' + line + ');\n' : code += 'r.push("' + line.replace(/"/g, '\\"') + '");\n';
     }
-	while(match = reg.exec(str)) {
+	while((match = reg.exec(str)) !== null) {
 	    add(str.slice(cursor, match.index));
 	    add(match[1], true);
 	    cursor = match.index + match[0].length;
 	}
     add(str.substr(cursor, str.length - cursor));
     code += 'return r.join("");';
+
+    // console.log(code)
+    // var r=[];
+    // r.push("Hello, my name is ");
+    // r.push(this.user.name);
+    // r.push(". I'm ");
+    // r.push(this.age);
+    // r.push(" years old.");
+    // return r.join("");
 
 	return new Function(code).apply(data);
 }
@@ -28,7 +38,7 @@ let res_0 = TemplateEngine(str,data)
 console.log(res_0)
 
 
-let StringTemplate = (str,data) => str.replace(/\${([^\}]+)?\}/g,val => new Function('return this.' + val.replace(/\${|\}/g,'')).call(data))
+let StringTemplate = (str,data) => str.replace(/\${([^\}]+)?\}/g,(val,p1) => new Function('return this.' + p1).call(data))
 
 let res_1 = StringTemplate(str,data)
 
